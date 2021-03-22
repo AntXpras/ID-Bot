2321a84b7ce9af8f9bd426c4531dc9b53321c402
AntXpras <80271241+AntXpras@users.noreply.github.com>
2021-03-22 09:49:35 +0700

    Update

--- idbot.py
+++ idbot.py
@@ -22,7 +22,7 @@ app = Client(
 if __name__ == "__main__":
     app.start()  # Not using run as wanna print
     uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+    print(f"@{uname} Started Berhasil...!")
     idle()
     app.stop()
-    print("Bot stopped. Alvida!")
+    print("Bot Berhenti. Hehe!")
