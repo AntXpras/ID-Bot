be14eed27b0301bc9c507e607e20d2bae37babfb
AntXpras <80271241+AntXpras@users.noreply.github.com>
2021-03-22 10:10:56 +0700

    Update
    Ya

--- Data.py
+++ Data.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     # Start Message
-    START = "Hey {}. \n\nWelcome to {} \n\nUsing this bot you can check id of any group, user, bot, channel and even stickers."
+    START = "Hallo {}. \n\nSelamat datang{} \n\nGunakan Bot ini untuk mendapatkan id grup, pengguna, bot, channel and dan id stiker."
 
     if Config.OWNER_ID != 0:
         if Config.OWNER_NAME:
@@ -14,16 +14,16 @@ class Data:
             )
         else:
             print(
-                "You added OWNER_ID but not OWNER_NAME. You need to put both or neithers"
+                "Kamu menambahkan OWNER_ID tetapi bukan OWNER_NAME. Kamu perlu mengambil both or neithers"
             )
-            print("Quitting the bot")
+            print("Keluar dari Bot")
             raise SystemExit
     else:
-        START += f"\n\nBy @MysteryBots ♥"
-    START += "\n\nP.S ~ Your ID is `{}`"
+        START += f"\n\nBot By @Cyntaxrobot 👻"
+    START += "\n\nP.S ~ ID kamu adalah`{}`"
 
     # About Message
-    ABOUT = "**About This Bot** \n\nThis is an open source ID bot by @MysteryBots \n\nSource : [Click Here](https://github.com/MysteryBots/ID-Bot) \n\nFramework : [Pyrogram](docs.pyrogram.org) \n\nLanguage : [Python](www.python.org) \n\nDeveloper : [Mყʂƚҽɾყ Bσყ](https://t.me/MysteryxD)"
+    ABOUT = "**Tentang Bot Ini** \n\nIni adalah bot opensource by @cyntaxrobot \n\nSource : [Click Here](https://telegram.me/cyntaxrobot) \n\nFramework : [Pyrogram](docs.pyrogram.org) \n\nLanguage : [Python](www.python.org) \n\nDeveloper : [Mყʂƚҽɾყ Bσყ] [Xpras_id](https://t.me/Xpras_id)"
 
     if Config.OWNER_ID != 0:
         if Config.OWNER_NAME:
@@ -38,21 +38,18 @@ class Data:
             raise SystemExit
 
     # Help Message
-    HELP = "**Help & Features** \n\n1) Send any message to get your ID. \n2) Forward any message from any user/bot/channel or anonymous admins to get ID. \n3) Send any sticker to get sticker id. \n4) Use Inline Mode to send your ID in any chat. \n5) Add in group / channel to get ID. \n6) Use /id command: \n- in private: To get ID through username \n- in group/channel: To get ID of that chat. \n\nBy @MysteryBots ♥"
-
-    # Deploy Message
-    DEPLOY = '**Wanna create your own such bot??** \n\nThis is simple and open source bot. \nJust click below on source code and tap on "Deploy to Heroku" to create your own bot. \n\nClick Here for [Source Code](https://github.com/MysteryBots/ID-Bot)'
+    HELP = "**Help & Features** \n\n1) Kirimkan Semua Pesan Untuk mendapatkan ID. \n2) Forward semua pesan user/bot/channel or anonymous admins to get ID. \n3) Send any sticker to get sticker id. \n4) Use Inline Mode to send your ID in any chat. \n5) Add in group / channel to get ID. \n6) Use /id command: \n- in private: To get ID through username \n- in group/channel: To get ID of that chat. \n\nBy @MysteryBots ♥"
 
     # Home Button
-    home_button = [[InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]]
+    home_button = [[InlineKeyboardButton(text="HO⬜ME", callback_data="home")]]
 
     # Rest Buttons
     buttons = [
         [
-            InlineKeyboardButton("✌ How to Use ✌", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about"),
+            InlineKeyboardButton("⬜ Cara Menggunakan ⬜", callback_data="help"),
+            InlineKeyboardButton("⬜ Tentang Bot ini ⬜", callback_data="about"),
         ],
-        [InlineKeyboardButton("Create your own bot", callback_data="deploy")],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/MysteryBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/MysteryBotsChat")],
+        
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/cyntaxrobot")],
+        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/Xpras_id")],
     ]
