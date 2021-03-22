6e438279185d7ade9b1b9f30c762ee78feef6857
AntXpras <80271241+AntXpras@users.noreply.github.com>
2021-03-22 09:43:50 +0700

    Update

--- ID-Bot/inline.py
+++ ID-Bot/inline.py
@@ -8,12 +8,12 @@ async def answer(idbot, inline_query):
 	await inline_query.answer(
 		results=[
 			InlineQueryResultArticle(
-				title=f"Your Telegram ID is {inline_query.from_user.id}",
+				title=f"✳ID Telegram Kamu{inline_query.from_user.id}",
 				input_message_content=InputTextMessageContent(
-					f"My Telegram ID is `{inline_query.from_user.id}`"
+					f"✳ID Telegram saya`{inline_query.from_user.id}`"
 				),
-				description="Tap to send your ID to current chat",
-				thumb_url="https://telegra.ph/file/784c14c76533f944ae9b0.jpg",
+				description="✳Kirimkan Pesan Untuk Mendapatkan ID✳",
+				thumb_url="https://telegra.ph/file/06234a45459a8befc04af.png",
 			)
 		],
 		cache_time=1,
